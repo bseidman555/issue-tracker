cb849ecefa9fbfd1727bacb36485ff9a1a841563
Brad Seidman <bseidman555@gmail.com>
2026-05-27 18:50:16 -0400

    Add simple MDE editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,19 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+"use client";
+
+import "easymde/dist/easymde.min.css";
+import { Button, TextField } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false, // This is the secret sauce that disables Server-Side Rendering
+  loading: () => <p>Loading Editor...</p>, // Optional placeholder while it loads
+});
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
