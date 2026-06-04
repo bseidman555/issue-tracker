bea85a8775899975a92a450a3d9a63f3d353a6f6
Brad Seidman <bseidman555@gmail.com>
2026-06-04 12:37:28 -0400

    Add react hook form to submit issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,15 @@
 "use client";
-
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import { Button, TextField } from "@radix-ui/themes";
 import dynamic from "next/dynamic";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false, // This is the secret sauce that disables Server-Side Rendering
@@ -10,12 +17,30 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
 });
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
