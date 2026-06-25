6a58ddf40eb9aa4517e0c0d65a8ff95737f90ded
Brad Seidman <bseidman555@gmail.com>
2026-06-25 14:32:53 -0400

    Create update issue api and modify issueform to handle the patch req

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,37 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "../../../validationSchemas";
+
+interface Props {
+    params: { id: string }
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+    const { id } = await params
+    const body = await request.json()
+
+    const validation = issueSchema.safeParse(body);
+
+    if (!validation.success)
+        return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(id)
+        }
+    });
+    if (!issue)
+        return NextResponse.json({ error: 'Invalid Issue Id' }, { status: 404 });
+
+    const updatedIssue = await prisma.issue.update({
+        where: {
+            id: issue.id
+        },
+        data: {
+            title: body.title,
+            description: body.description
+        }
+    });
+
+    return NextResponse.json(updatedIssue, { status: 201 })
+}
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,13 +7,13 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { Issue } from "@prisma/client";
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false, // This is the secret sauce that disables Server-Side Rendering
@@ -28,14 +28,15 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setSubmitting(false);
@@ -66,7 +67,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
