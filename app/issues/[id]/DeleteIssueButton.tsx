25467e8efe14e29225710355f06165b6721c4922
Brad Seidman <bseidman555@gmail.com>
2026-06-30 13:47:24 -0400

    Add confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,31 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
-
+//  <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
 const DeleteIssueButton = async ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red"></Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this Issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
